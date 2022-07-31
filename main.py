0fbaececaf6c066f807ed36e4d0c91a543172741
Abrar1S <levihanji09@gmail.com>
2022-07-31 14:04:24 +0600

    Flask app added

---
+++ main.py
@@ -0,0 +1,46 @@
+'''importing Flask and other modules'''
+from flask import Flask, request, render_template
+from joblib import load
+
+
+app = Flask(__name__)
+
+# load the model  
+model = load("models/model_v1.joblib")
+
+
+@app.route('/', methods=["GET", "POST"])
+def classification():
+
+    form_items = ['Age', 'Sex', 'BP', 'Cholesterol','Na_to_k']
+    data = []
+    pred = None
+    if request.method == "POST":
+        # getting input with name = fname in HTML form
+        for item in form_items:
+            try:
+                temp = request.form.get(item)
+                temp = temp.strip()
+                temp = int(temp)
+                data.append(temp)
+            except Exception as ex:
+                print(ex)
+        print(data)
+        pred = model.predict([data])
+        if pred[0] == 0:
+            pred = "DrugY"
+        elif pred[0] == 1:
+            pred = "drugA"
+        elif pred[0] == 2:
+            pred = "drugB"
+        elif pred[0] == 3:
+            pred = "drugC"
+        elif pred[0] == 4:
+            pred = "drugX"
+
+    return render_template("home.html", result=pred)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True)
+   
